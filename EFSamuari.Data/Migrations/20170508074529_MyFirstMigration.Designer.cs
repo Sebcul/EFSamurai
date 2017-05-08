9726ca201d91ff797a3d31377bf157c666ef348b
sebcul <cullbrand@gmail.com>
2017-05-08 10:04:06 +0200

    Init commit

---
+++ EFSamuari.Data/Migrations/20170508074529_MyFirstMigration.Designer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using EFSamuari.Data;
+
+namespace EFSamuari.Data.Migrations
+{
+    [DbContext(typeof(SamuraiContext))]
+    [Migration("20170508074529_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("EFSamurai.Domain.Samurai", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Samurais");
+                });
+        }
+    }
+}
---
+++ EFSamuari.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using EFSamurai.Domain;
+
+namespace EFSamuari.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Server = (localdb)\\mssqllocaldb; Database = EfSamurai; Trusted_Connection = True; ");
+        }
+    }
+}
